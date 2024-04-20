d656de8fa1a88300d9eba5c21eb5446dabdd6b9f
Hussain Alnakhli <mah.hussain@hotmail.com>
2024-04-20 14:39:42 -0400

    new design (dark mode), added date, fixed bugs

--- App.js
+++ App.js
@@ -13,10 +13,30 @@ import {
 } from "react-native";
 import Task from "./components/Task";
 export default function App() {
+  const currentDate = new Date();
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = monthNames[currentDate.getMonth()];
+  let day = currentDate.getDate();
+  let year = currentDate.getFullYear();
+  let formattedDate = `${month} ${day} ${year}`;
   const [task, setTask] = useState();
   const [taskItems, setTaskItems] = useState([]);
   const handleAddTask = () => {
     Keyboard.dismiss();
+
     setTaskItems([...taskItems, task]);
     setTask(null);
   };
@@ -28,58 +48,67 @@ export default function App() {
   };
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-    <View style={styles.container}>
-      <View style={styles.taskWrapper}>
-        <Text style={styles.sectionTitle}>Today's Tasks</Text>
-        <View style={styles.items}>
-          {/* Tasks */}
-          {taskItems.map((item, index) => {
-            return (
-              <TouchableOpacity onPress={() => completeTask(index)}>
-                <Task key={index} text={item} />
-              </TouchableOpacity>
-            );
-          })}
-        </View>
-      </View>
-
-      {/* Write a task */}
-
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={styles.writeTaskWrapper}
-      >
-        <TextInput
-          style={styles.input}
-          placeholder={"Write a task"}
-          value={task}
-          onChangeText={(text) => setTask(text)}
-        />
-        <TouchableOpacity onPress={() => handleAddTask()}>
-          <View style={styles.addWrapper}>
-            <Text style={styles.addText}>+</Text>
+      <View style={styles.container}>
+        <View style={styles.taskWrapper}>
+          <Text style={styles.sectionTitle}>Today's Tasks</Text>
+          <Text style={styles.sectionDate}>{formattedDate}</Text>
+          <View style={styles.items}>
+            {/* Tasks */}
+            {taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity onPress={() => completeTask(index)}>
+                  <Task key={index} text={item} />
+                </TouchableOpacity>
+              );
+            })}
           </View>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
+        </View>
 
-      <StatusBar style="auto" />
-    </View>
+        {/* Write a task */}
+
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={styles.writeTaskWrapper}
+        >
+          <TextInput
+            style={styles.input}
+            placeholder={"Write a task"}
+            placeholderTextColor="#C1C1C1"
+            
+            value={task}
+            onChangeText={(text) => setTask(text)}
+          />
+          
+          <TouchableOpacity onPress={() => handleAddTask()}>
+            <View style={styles.addWrapper}>
+              <Text style={styles.addText}>+</Text>
+            </View>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </View>
     </TouchableWithoutFeedback>
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#DBE6F5",
+    backgroundColor: "#141414",
   },
   taskWrapper: {
     paddingTop: 80,
     paddingHorizontal: 20,
   },
   sectionTitle: {
+    color: "white",
     fontSize: 24,
     fontWeight: "bold",
+  },
+  sectionDate: {
+    color: "gray",
+    fontSize: 15,
+    
   },
   items: {
     marginTop: 30,
@@ -96,20 +125,26 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 15,
     width: 250,
-    backgroundColor: "white",
+    backgroundColor: "#272727",
     borderRadius: 60,
-    borderColor: "gray",
+    borderColor: "#272727",
     borderWidth: 1,
+    fontWeight:"bold",
+    color:"white"
   },
   addWrapper: {
     width: 60,
     height: 60,
-    backgroundColor: "white",
+    backgroundColor: "#272727",
     borderRadius: 60,
-    borderColor: "gray",
+    borderColor: "#272727",
     borderWidth: 1,
     alignItems: "center",
     justifyContent: "center",
   },
-  addText: {},
+  addText: {
+    fontSize:30,
+    color:"#D2D0D0"
+
+  },
 });
--- components/Task.js
+++ components/Task.js
@@ -1,54 +1,68 @@
 import React from "react";
 import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 
-const Task = (props) =>{
+const Task = (props) => {
+  // Get the current date
+  let currentDate = new Date();
 
+  // Get hours and minutes
+  let hours = currentDate.getHours();
+  let minutes = currentDate.getMinutes();
 
-    return (
-        <View style={styles.item}>
-            <View style={styles.itemLeft}>
-                <View style={styles.sqaure}></View>
-                <Text style={styles.itemText}>{props.text}</Text>
-            </View>
-            <View style={styles.circle}></View>
-        </View>
-    )
-}
+  // Convert hours to 12-hour format
+  let ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // 12-hour clock, so 0 becomes 12
+
+  // Add leading zero to minutes if necessary
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+
+  // Format the time
+  let formattedTime = `${hours}:${minutes} ${ampm}`;
+
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <View style={styles.circle}></View>
+        <Text style={styles.itemText}>{props.text}</Text>
+      </View>
+      <View>
+        <Text style={styles.sqaure}>{formattedTime}</Text>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-
-    item:{
-        backgroundColor:"white",
-        padding:15,
-        borderRadius:10,
-        flexDirection: "row",
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        marginBottom:20,
-    },
-    itemLeft:{
-        flexDirection: "row",
-        alignItems: 'center',
-        flexWrap: 'wrap'
-    },
-    itemText:{
-        maxWidth: "80%"
-    },
-    sqaure:{
-        width:24,
-        height:24,
-        backgroundColor:"blue",
-        opacity:0.4,
-        borderRadius:5,
-        marginRight:15,
-    },
-    circle:{
-        width:12,
-        height:12,
-        borderColor:"blue",
-        borderWidth: 1,
-        borderRadius: 5,
+  item: {
+    backgroundColor: "#272727",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 8,
   },
-})
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  itemText: {
+    maxWidth: "80%",
+    color: "#DEDEDE",
+  },
+  sqaure: {
+    color:"#D2D0D0"
+  },
+  circle: {
+    width: 12,
+    height: 12,
+    borderColor: "white",
+    borderWidth: 1,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+});
 
 export default Task;
